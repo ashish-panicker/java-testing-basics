7698c8329baae2b66d44cdf6e4c0651cbe5c4ad2
ashish-panicker <ashish.s.panicker@proton.me>
2025-01-15 15:45:16 +0530

    Unit test with code coverage

---
+++ src/test/java/com/example/PassengerTest.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PassengerTest {
+
+  @Test
+  @DisplayName("Test Passenger class")
+  void testPassenger() {
+    var passenger = new Passenger("John Doe",
+        "john.doe",
+        LocalDate.of(1980, 10, 20),
+        Gender.MALE,
+        4000,
+        500,
+        TravelPass.SERVICE_MEMBER);
+
+    assertEquals("John Doe", passenger.getFullName());
+    assertEquals("john.doe", passenger.getEmail());
+    assertEquals(LocalDate.of(1980, 10, 20), passenger.getDateOfBirth());
+    assertEquals(Gender.MALE, passenger.getGender());
+    assertEquals(4000, passenger.getPrice());
+    assertEquals(500, passenger.getTravelPoints());
+    assertEquals(TravelPass.SERVICE_MEMBER, passenger.getTravelPass());
+
+  }
+}
